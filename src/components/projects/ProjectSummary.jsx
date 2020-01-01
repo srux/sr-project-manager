9b7a89ee9d8b70518ddbcdfbe2fbf6758a0f552f
srux <samrob.nz@gmail.com>
2020-01-01 15:22:48 +1300

    v1 complete

--- src/components/projects/ProjectSummary.jsx
+++ src/components/projects/ProjectSummary.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import moment from 'moment';
 
 const ProjectSummary = ({project}) => {
     return (
         <div className="card z-depth-0 project-summary">
             <div className="card-content grey-test text-darken-3">
                 <span className="card-title">{project.title}</span>
-                <p>Posted by SRUX</p>
-                <p className="grey-text">30 December, 3.13pm</p>
+                <p>Posted by {project.authorFirstName} {project.authorLastName}</p>
+                <p className="grey-text">{moment(project.createdAt.toDate()).calendar()}</p>
             </div>
         </div>
     );
--- src/store/actions/projectActions.jsx
+++ src/store/actions/projectActions.jsx
@@ -3,11 +3,14 @@ export const createProject = (project) => {
         // make async call to db
 
         const firestore = getFirestore();
+        const profile = getState().firebase.profile;
+        const authorId = getState().firebase.auth.uid;
+
         firestore.collection('projects').add({
             ...project,
-            authorFirstName:'SR',
-            authorLastName:'UX',
-            authorId:'12345',
+            authorFirstName:profile.firstName,
+            authorLastName:profile.lastName,
+            authorId:authorId,
             createdAt: new Date()
         }).then(() => {
             dispatch({ type: 'CREATE_PROJECT',project});
